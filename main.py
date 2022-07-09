88f6fa5c5e7ac449629cc475f0176f2bc65be370
zalivo <izalodeuk5@gmail.com>
2022-07-09 15:37:14 +0200

    maybe the assigned will work

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ auth.set_access_token(keys.access_token, keys.access_secret_token)
 # API
 api = tweepy.API(auth)
 
-#Credentials check
+# Credentials check
 try:
     api.verify_credentials()
     print("Everything good")
 except:
     print("Something's wrong")
 
+
+def letsTweet():
+    if date.today().weekday() == 0:
+        tweet_logic.imageTweet()
+    elif date.today().weekday() == 2:
+        tweet_logic.textTweet()
+
+# You Tweet here finally
+letsTweet()
 
